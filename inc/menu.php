9312bea2f16285e265de1b11c4cd5c908754cf75
TOHOU Orphée <orpheetohou16@gmail.com>
2022-07-01 20:23:04 +0200

    Ajout des dossiers

---
+++ inc/menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white " href="#"><span>E</span>-shop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center " id="navbarSupportedContent">
+      <ul class="navbar-nav ">
+        <li class="nav-item mr-2">
+          <a class="nav-link active text-white " aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="#">Produits</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Vente</a></li>
+            <li><a class="dropdown-item" href="#">Achat</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Paiement</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Contact</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
